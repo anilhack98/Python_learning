82b1f8f87905fa98ba3c3647789f1a4b520c234a
anilhack98 <chaudhary98.sc@gmail.com>
2025-06-17 06:21:56 +0545

    day7 added

---
+++ day8/creatcalc.py
@@ -0,0 +1,6 @@
+a=45
+b=20
+print("The sum of", a, "+", b, "is:",a+b)
+print("The difference of", a, "-", b, "is:",a-b)
+print("The product of", a, "*", b, "is:",a*b)
+print("The quotient of", a, "/", b, "is:",a/b)
