95f953284c074090112ec152af46e3d626dd4cf0
anilhack98 <chaudhary98.sc@gmail.com>
2025-06-25 00:08:23 +0545

    added

---
+++ day18/while_loop.py
@@ -0,0 +1,12 @@
+i=0
+while(i<=45):
+    i=int(input("Enter a number: "))
+    print(i)
+print("You have entered a number greater than 45, exiting the loop.")
+
+# Another Example
+count=10
+while(count>0):
+    print(count)
+    count=count-1
+print("Countdown finished!")
