ee66838e89faaa307ccbddfab3f417301141616f
anilhack98 <chaudhary98.sc@gmail.com>
2025-06-29 18:06:46 +0545

    commit

--- day22/list.py
+++ day22/list.py
@@ -1,10 +1,27 @@
-list=[45,34,50]
+list=[45,34,50,"Anil",True,67.89,41,55,67,78,90]
 print(list)
 print(type(list))
 print(list[0])  # Accessing the first element of the list
 print(list[1])  # Accessing the second element of the list
 print(list[2])  # Accessing the third element of the list
+print(list[3])  # Accessing the fourth element of the list
+print(list[4])  # Accessing the fifth element of the list
+print(list[5])  # Accessing the sixth element of the list
+print(list[6])  # Accessing the seventh element of the list
+print(list[7])  # Accessing the eighth element of the list
+print(list[8])  # Accessing the ninth element of the list
+print(list[9])  # Accessing the tenth element of the list
 
 # Negative Indexing
 print(list[-3])
 print(len(list)-3)  # This will give the same result as list[-3]
+
+print(list)
+print(list[1:8])
+print(list[1:8:2])  # Slicing the list from index 1 to 7 with a step of 2
+
+lst=[i*i for i in range(10)]
+print(lst)
+
+lst1=[i*i for i in range(10) if i%2==0]
+print(lst1)
---
+++ day23/list_method.py
@@ -0,0 +1,17 @@
+l=[4,5,10,143,23,45,67,89,90,10,10]
+# print(l)
+# l.append(100)  # Adding an element to the end of the list
+# l.sort()  # Sorting the list in ascending order
+# l.reverse()  # Reversing the order of the list
+# print(l.index(10)) # Finding the index of the first occurrence of 10
+# print(l.count(10))  # Counting the occurrences of 10 in the list
+# m=l.copy()  # Creating a copy of the list
+# m[0]=0
+# print(m)  # Printing the copied list
+
+l.insert(2, 200)  # Inserting 200 at index 2
+
+p=[34, 45, 67]
+l.extend(p) # Extending the list with elements from another list
+print(l)
+
