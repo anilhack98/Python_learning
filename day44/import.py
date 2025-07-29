bad663572038fac0a936083b5178f21049253600
Anil Chaudhary <chaudhary98.sc@gmail.com>
2025-07-29 09:51:30 +0545

    commit

---
+++ day44/import.py
@@ -0,0 +1,22 @@
+# from math import sqrt,pi
+from math import *  # * is used to import all function and variables from the math module
+result=sqrt(9)*pi
+print(result)
+
+
+# The "As" Keyword
+# The "as" keyword is used to assign an alias to an expression. It is used to
+import math as m
+result=m.sqrt(8)*m.pi
+print(result)
+
+
+# The "Dir" function
+# The "dir" function is used to return a list of valid attributes for that object.
+
+# from anil import welcome,anil
+from anil import *  # next method to import using *
+import math
+print(dir(math))
+welcome()
+print(anil)
