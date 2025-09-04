5ea69cc70fdc34933ec4fe140506bc357a0d4529
Anil Chaudhary <chaudhary98.sc@gmail.com>
2025-09-04 21:48:22 +0545

    commit

--- day61/inheritance.py
+++ day61/inheritance.py
@@ -5,6 +5,12 @@ class Employee:
 
     def showDetails(self):
         print(f"The name of Employee is {self.name} and his id is {self.id}")
+class Programmer(Employee):
+    def showLanguage(self):
+        print("The default Language is Python")
 
 e=Employee("Ram",205)
 e.showDetails()
+e1=Programmer("Hari",203)
+e1.showDetails()
+e1.showLanguage()
